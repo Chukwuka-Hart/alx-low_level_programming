40baa2ab56050e54661f1d74dd5147c9981a0f1e
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-17 15:52:34 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry gate
+ *
+ * Details: Print numbers seperated by space and comma
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+int d = 0;
+
+while (d < 10)
+{
+putchar(d + '0');
+d = d + 1;
+if (d < 10)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+
+return (0);
+}
