124330b1bb511b4199229ee422fee6a3d272028c
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-17 19:39:31 -0700

    deleted excess file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main 
+ * main
  *
  * code
  *
@@ -19,7 +19,7 @@ for (d = 0; d <= 98; d++)
 {
 for (e = d + 1; e <= 99; e++)
 {
-putchar(( d / 10) + '0');
+putchar((d / 10) + '0');
 putchar((d % 10) + '0');
 putchar(' ');
 putchar((e / 10) + '0');
@@ -28,8 +28,8 @@ if (d == 98 && e == 99)
 break;
 putchar(',');
 putchar(' ');
-}    
-}  
+}
+}
 putchar('\n');
 return (0);
 }
