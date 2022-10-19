5ad547668ce4c430887414e5f5add914dbf7e2be
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 10:04:56 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,14 @@
 /**
  * _islower - checks for lowercase characters
  *
- *c: char to be checked
+ * c: char to be checked
  *
  * Return: 1 if lowercase, 0 if otherwise
  */
 
 int _islower(int c)
 {
-  if (_islower(c) <= 'z')
+if (c >= 'a'; && c <= 'z')
 {
 return (1);
 }
