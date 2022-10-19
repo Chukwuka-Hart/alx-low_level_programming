ac6c242d92114a302bb6ccba38be4022544043f8
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 06:58:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <unistd.h>
+
+/**
+ * FILE: main.h
+ *
+ * DETAILS: header file containing all the functions used
+ *
+ */
+
+ void _putchar(char d)
+ {
+   write(1, &d, 1);
+ }
+
+
+#endif
