06be035dad268389fc0f7499d870df1038ce1b50
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 19:42:39 -0700

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural number
+ *
+ * @n: number
+ */
+
+void print_to_98(int n)
+{
+while (n <= 98)
+{
+printf("%d, ", n);
+n++;
+}
+while (n >= 98)
+{
+printf("%d, ", n);
+n--;
+}
+printf("\n");
+}
