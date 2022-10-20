69a7785b994b5c5d0fc1d7703abc205c15c2b8b7
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 21:33:43 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,30 +11,24 @@ void print_times_table(int n)
 int c, d, e;
 
 if (n >= 0 && n <= 15)
-
 {
 
 for (c = 0; c <= n; c++)
-
 {
-
 _putchar('0');
 
 for (d = 1; d <= n; d++)
 {
 
 _putchar(',');
-
 _putchar(' ');
 
 e = c * d;
 
 if (e <= 99)
-
 _putchar(' ');
 
 if (e <= 9)
-
 _putchar(' ');
 
 if (e >= 100)
@@ -44,13 +38,11 @@ _putchar((e / 10) % 10 +  '0');
 }
 else if (e <= 99 && e >= 10)
 {
-
 _putchar((e / 10) + '0');
 }
-
 _putchar((e % 10) + '0');
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
 }
