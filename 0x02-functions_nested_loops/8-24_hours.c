3799867c38411d01d495404ebc1d4cfeceabb452
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 18:16:31 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,11 @@
 void jack_bauer(void)
 {
 int hour = 0;
-int mins = 0;
 
 while (hour < 24)
 {
+int mins = 0;
+
 while (mins < 60)
 {
 _putchar((hour / 10) + '0');
@@ -24,8 +25,9 @@ _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((mins / 10) + '0');
 _putchar((mins % 10) + '0');
-_putchar('\n');
+mins++;
 }
+_putchar('\n');
 hour++;
 }
 }
