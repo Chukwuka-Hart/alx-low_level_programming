9460320ee4f431f6af131373eaaa63fc7a321aa3
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-19 19:06:55 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table
+ *
+ */
+
+void times_table(void)
+{
+int x, y, z;
+
+for (x = 0; x < 10; x++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+for (y = 1; y < 10; y++)
+{
+z = x * y;
+if ((z / 10) > 0)
+{
+_putchar((z / 10) + '0');
+}
+else
+{
+_putchar(' ');
+}
+_putchar((z % 10) + '0');
+
+if (y < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+
+}
+}
+_putchar('\n');
+}
