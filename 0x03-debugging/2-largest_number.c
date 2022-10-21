2077b6f504c3fd431cc60c663679d2a0913fc902
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-20 18:41:52 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (c > b)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
