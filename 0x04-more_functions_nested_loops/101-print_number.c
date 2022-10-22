fb57cf7262384c00cd9da2d77414e807a48b4de9
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-21 21:05:16 -0700

    12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: int to be printed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int c;
+
+	c = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		x + -n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
