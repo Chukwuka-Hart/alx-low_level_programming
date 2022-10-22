e43822ebfd5f876b834a44100f9ac53eb078d5a2
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-21 18:24:36 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 	int c;
 	int d;
 
-	for (c = 0; c < 10; c++)
+	for (c = '0'; c < '10'; c++)
 	{
-		for (d = 0; d <= 14;)
+		for (d = '0'; d <= '14';)
 		{
 			_putchar(d);
 		}
