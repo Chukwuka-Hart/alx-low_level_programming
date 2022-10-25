50fb6b7b137a2faa6f7f9d3715748761ec5e7171
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-25 11:03:22 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenth if a string
+ *
+ * @s: string
+ *
+ * Return: strlen
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = _strlen(s);
+
+	return (length);
+}
