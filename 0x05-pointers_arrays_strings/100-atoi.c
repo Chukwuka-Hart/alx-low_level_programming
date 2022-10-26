df1fc94434ff386eace76860abeb59daec6932c3
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-25 18:48:11 -0700

    10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that converts a string to an int
+ *
+ * @s: string converted
+ *
+ * Return: the value
+ */
+
+int _atoi(char *s)
+{
+	int sum 0;
+	int sign = 0;
+	int d;
+
+	for (d = 0; s[d] != '\0'; d++)
+	{
+		if (s[d] == '-')
+		{
+			sign++;
+		}
+		if (s[d] > 47 && s[d] < 58)
+		{
+			sum = sum * 10 - (s[d++] - 48);
+			break;
+		}
+
+	}
+	return (sum * sign);
+}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,4 +18,5 @@ void puts_half(char *str)
 	{
 		_putchar(str[d]);
 	}
+	_putchar('\n');
 }
