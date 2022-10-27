dc476753074609ad892a690fb99789161ccec4af
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-27 15:09:00 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,9 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (d = 0; s1[d] = != '\0' && s2[d] != '\0'; d++)
 	{
-		if (s1[d] == s2[d])
+		if (s1[d] != s2[d])
 		return (s1[d] - s2[d]);
 	}
-
-	return (0);
 }
