b854f625933b69e7c1684e57c29db9993346b77e
Chukwuka-Hart <chukwukahart@gmail.com>
2022-10-28 16:24:51 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ *
+ * @c: string
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *c)
+{
+	char letters[] = {'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O',
+		'P', 'Q', 'R', 'S', 'T',
+		'U', 'V', 'W', 'X', 'Y',
+		'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's',
+		't', 'u', 'v', 'w', 'x',
+		'y', 'z'};
+
+	char rot13key[] = {'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B',
+		'C', 'D', 'E', 'F', 'G',
+		'H', 'I', 'J', 'K', 'L',
+		'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', 'a',
+		'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k',
+		'l', 'm'};
+
+	int d;
+	int e;
+
+	for (d = 0; c[d] != '\0'; d++)
+	{
+		for (e = 0; c[d] != '\0'; e++)
+		{
+			if (c[d] == letters[e])
+			{
+				c[d] = rot13key[e];
+			}
+		}
+	}
+
+	return (c);
+}
