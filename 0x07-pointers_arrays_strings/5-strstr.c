07dc837a8af733f626b5b95879ab8a595b541428
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-01 14:58:27 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * strstr - locates a substring
+ *
+ * @haystack: string to search in
+ * @needle: an input string to locate in 
+ *
+ * Return: a pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char x, s = 0;
+
+	while (needle[s] != '\0')
+		s++;
+
+	while (*haystack)
+	{
+		for (x = 0; needle[x]; x++)
+		{
+			if (haystack[x] != needle[x])
+				break;
+		}
+
+		if (x != 0)
+			haystack++;
+		else
+			return (haystack);
+	}
+
+	return (0);
+}
