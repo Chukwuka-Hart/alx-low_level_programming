905df4cdd3028b72f5f19ed3617667c36f01ce5e
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-02 20:45:25 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 {
 	int x;
 
-	if (*s == '\0')
+	if (*s)
 	{
 		return (0);
 	}
@@ -20,6 +20,7 @@ int _strlen_recursion(char *s)
 	else
 	{
 		x = _strlen_recursion(s + 1);
+		x++;
 	}
 
 	return (x);
