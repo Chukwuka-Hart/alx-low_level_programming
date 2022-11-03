574a216d57c76ae65732a5dca5356fdb2ba09bb8
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-03 02:45:46 -0700

    5 correction

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _findrt - finds the natural square root of an int
+ *
+ * @x: int to be tested
+ * @y: int to find
+ *
+ * Return: if the int has natural sqrt 1, otherwise -1
+ */
+
+int _findrt(int x, int y)
+{
+	if ((x * x) == y)
+		return (x);
+	if (y == x / 2)
+		return (-1);
+	return (_findrt(y, x + 1));
+}
+
+
+/**
  * _sqrt_recursion - returns the square root of a number
  *
  * @n: int
@@ -10,4 +29,5 @@
 
 int _sqrt_recursion(int n)
 {
-
+	return (_findrt(n, y));
+}
