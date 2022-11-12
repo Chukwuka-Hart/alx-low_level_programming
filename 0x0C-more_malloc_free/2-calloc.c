0c4f21af9339749c5ce56479efff798b4ce816ca
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-11 18:18:38 -0800

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - ...
+ *
+ * @nmeb: ...
+ * @size: ...
+ *
+ * Return: ...
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int c = 0;
+	int s = 0;
+	char *x;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = nmeb * size;
+	x = malloc(s);
+
+	if (x == NULL)
+		return (NULL);
+
+	while (c < s)
+	{
+		x[c] = 0;
+		c++;
+	}
+
+	return (x);
+}
