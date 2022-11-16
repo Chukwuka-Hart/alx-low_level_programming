d2e60436e090ee1c6958156fe3d28598f4766f16
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-15 20:52:02 -0800

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,7 @@
 void print_dog(struct dog *d)
 {
 
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+	if (d != NULL)
 	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
 	printf("Age: %f\n", (d->age));
 	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
