9f804af862933374c91283ce96cd2ca833f3f5e3
Chukwuka-Hart <chukwukahart@gmail.com>
2022-11-30 17:35:48 -0800

    4&5

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -35,6 +35,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp->next = newlist;
 		return (temp);
 	}
-	
+
 	return (NULL);
 }
---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of list
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
